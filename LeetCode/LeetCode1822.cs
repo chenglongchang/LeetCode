e6dba7da92b9996a66fb418b7112c44c6b8c663f
zcl <zcl@DESKTOP-HKVQ5PG>
2022-10-29 15:33:31 +0800

    添加项目文件。

---
+++ LeetCode/LeetCode1822.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public static class LeetCode1822
+    {
+        public static int ArraySign(int[] nums)
+        {
+            bool negativeFlag;
+            negativeFlag = false;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 0)
+                {
+                    return 0;
+                }
+                else if (nums[i] < 0)
+                {
+                    negativeFlag = !negativeFlag;
+                }
+            }
+
+            return negativeFlag ? -1 : 1;
+        }
+
+        public static int Test()
+        {
+            int[] nums = new int[7] { -1, -2, -3, -4, 3, 2, 1 };
+
+            return ArraySign(nums);
+        }
+    }
+}
---
+++ LeetCode/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace LeetCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(LeetCode1822.Test().ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
