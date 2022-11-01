8d4e58656418c5ba5fc8b13755e1c40bb45f3da9
zcl <2436622503@qq.com>
2022-11-01 20:22:07 +0800

    add LeetCode1662

---
+++ LeetCode/LeetCode1662.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public static class LeetCode1662
+    {
+        public static bool ArrayStringsAreEqual(string[] word1, string[] word2)
+        {
+            return string.Join("", word1).Equals(string.Join("", word2));
+        }
+    }
+}
